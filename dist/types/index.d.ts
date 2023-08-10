b9f75dda18cc24fa531c60bfc5a5e6876a567076
Yujan Prajapati <yujan@conspireagency.com>
2023-08-10 14:33:54 +0545

    Fix

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,3 +1,2 @@
-export * from './dissolve/utils';
 export * from './elements';
 export * from './types';
--- src/dissolve/utils.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-  return date.toLocaleDateString('en-US', options);
-};
