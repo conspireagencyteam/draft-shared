a55027fa3e179c0f3c696555fdcfae4749bcbef2
Daniel Andrade <Daniel@conspireagency.com>
2023-09-29 16:59:55 -0700

    added default settings, built and updated the package version

--- dist/index.js
+++ dist/index.js
@@ -16,3 +16,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./elements"), exports);
 __exportStar(require("./types"), exports);
+__exportStar(require("./default-settings"), exports);
